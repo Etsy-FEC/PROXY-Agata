a2e27377b955456a36d79e0b1c2db19944f8b344
agatadrozdowska <agata.drozdowska@icloud.com>
2020-02-05 11:55:07 -0600

    Created index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static("public"));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
